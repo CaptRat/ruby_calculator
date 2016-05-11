f74a5ec4d094c19e3d7043136fddf43f9c519e40
Patrick Logan <jpatricklogan@gmail.com>
2016-05-11 12:07:12 -0600

    pseudocode for basic calculator

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,20 @@
+# create /edit a file
+# stage the file - git add
+# Make a commit
+# push to a github repo
+
+
+
 puts "Welcome to the Ruby calculator"
 puts "Enter your first number:"
+
+# get the first number
+puts "Enter your operator"
+# get the operator
+puts "Enter the second number:"
+# get the second number
+
+# figure out how to do the math on number
+# output the result to the user
+
+# Do all the bonus problems
